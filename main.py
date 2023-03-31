7148ec1da7e852b07da76f3ee6017dc93cf67a7d
fouad mahmoud <100407343+fouadmahmoud281@users.noreply.github.com>
2023-03-31 11:42:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+name = input("you name?")
+food = input("please add your favoruite food?")
+music = input("your lovely song?")
+print("Good Morning ," + name + " we orderd for you lovely " + food + " and will turn " + music + " song for you in the room")
